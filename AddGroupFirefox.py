37d73eb868e6cceb290299ce39ce7fe08ee7561d
mk01-v <prizma5150@gmail.com>
2019-11-11 16:08:17 +0700

    преобразование кода: 1. login - добавлены параметры username, password - являются универсальными. 2. create_group - namegroup, header - являются универсальными.

--- AddGroupFirefox.py
+++ AddGroupFirefox.py
@@ -15,9 +15,9 @@ class AddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username ="admin", password ="secret")
         self.open_group_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, namegroup ="group_name_cool", header ="group_header_cool_logo")
         self.return_to_group_page(wd)
         self.logout(wd)
 
@@ -29,16 +29,16 @@ class AddGroup(unittest.TestCase):
         # return groups page
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, namegroup, header):
         # init group creation
         wd.find_element_by_name("new").click()
         # filling group
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group_name_cool")
+        wd.find_element_by_name("group_name").send_keys(namegroup)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("group_header_cool_logo")
+        wd.find_element_by_name("group_header").send_keys(header)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -46,15 +46,15 @@ class AddGroup(unittest.TestCase):
         # open groups page
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
 
