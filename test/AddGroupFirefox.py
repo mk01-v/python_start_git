9345e7f9e2f0cf9780c5fbe37c9288137b1b7816
mk01-v <prizma5150@gmail.com>
2019-11-12 15:47:56 +0700

    Refactor->Rename: create_group в create, т.к. нечитабельно.

--- test/AddGroupFirefox.py
+++ test/AddGroupFirefox.py
@@ -11,11 +11,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username ="admin", password ="secret")
-    app.group.create_group(Group(name="group_name_cool", header="group_header_cool_logo"))
+    app.group.create(Group(name="group_name_cool", header="group_header_cool_logo"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username ="admin", password ="secret")
-    app.group.create_group(Group(name="", header=""))
+    app.group.create(Group(name="", header=""))
     app.session.logout()
 
