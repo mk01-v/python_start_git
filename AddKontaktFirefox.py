8e609d95cd28d647403ca84dd91fd02d9e22829e
mk01-v <prizma5150@gmail.com>
2019-11-12 10:40:04 +0700

    добавлено: импорт контакт.

--- AddKontaktFirefox.py
+++ AddKontaktFirefox.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from kontakt import kontakt
 import unittest, time, re
 
 class AddKontaktFirefox(unittest.TestCase):
@@ -16,78 +17,66 @@ class AddKontaktFirefox(unittest.TestCase):
         driver = self.driver
         self.open_home_page(driver)
         self.login(driver, admin="admin", password="secret")
-        self.add_new_record(driver, username="First_name: Artur", middle_name="Middle_name: 123", last_name="Last_name: Piroshkov", nickname="Nickname: 123",
-                            title="Title: addressbook", company="Company: OOO Privet", address="Address: Michurina 3", home="Home: 3",
-                            mobile="Mobile: 9232847147", work="Work: None", fax="Fax: None", email="E-mail: 123@gmail.com",
-                            email2="E-mail2: 234@gmail.com", email3="E-mail3: 345@gmail.com", homepage="Homepage: None", bday="28", bmonth="July", byear="1992",
-                            aday="1", amonth="January", ayear="2000", secondary_address2="Secondary-address: Michurina 10", secondary_home2="Secondary-home: 10",
-                            secondary_notes="Secondary-notes: None")
+        self.add_new_record(driver, kontakt)
         self.logout(driver)
 
     def test_add_kontakt_firefox2(self):
         driver = self.driver
         self.open_home_page(driver)
         self.login(driver, admin="admin", password="secret")
-        self.add_new_record(driver, username="", middle_name="", last_name="", nickname="",
-                            title="", company="", address="", home="",
-                            mobile="", work="", fax="", email="",
-                            email2="", email3="", homepage="", bday="28", bmonth="July", byear="1992",
-                            aday="1", amonth="January", ayear="2000", secondary_address2="", secondary_home2="",
-                            secondary_notes="")
+        self.add_new_record(driver, kontakt)
         self.logout(driver)
 
     def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
 
-    def add_new_record(self, driver, username, middle_name, last_name, nickname, title, company, address, home, mobile,
-                       work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
-                       secondary_address2, secondary_home2, secondary_notes):
+    def add_new_record(self, driver, kontakt):
         driver.find_element_by_link_text("add new").click()
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(username)
+        driver.find_element_by_name("firstname").send_keys(kontakt.username)
         driver.find_element_by_name("middlename").click()
         driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(middle_name)
+        driver.find_element_by_name("middlename").send_keys(kontakt.middle_name)
         driver.find_element_by_name("lastname").click()
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(last_name)
+        driver.find_element_by_name("lastname").send_keys(kontakt.last_name)
         driver.find_element_by_name("nickname").click()
         driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys(nickname)
+        driver.find_element_by_name("nickname").send_keys(kontakt.nickname)
         driver.find_element_by_name("title").click()
         driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys(title)
+        driver.find_element_by_name("title").send_keys(kontakt.title)
         driver.find_element_by_name("company").click()
         driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys(company)
+        driver.find_element_by_name("company").send_keys(kontakt.company)
         driver.find_element_by_name("address").click()
         driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys(address)
+        driver.find_element_by_name("address").send_keys(kontakt.address)
         driver.find_element_by_name("home").click()
         driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys(home)
+        driver.find_element_by_name("home").send_keys(kontakt.home)
         driver.find_element_by_name("mobile").click()
         driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys(mobile)
+        driver.find_element_by_name("mobile").send_keys(kontakt.mobile)
         driver.find_element_by_name("work").click()
         driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys(work)
+        driver.find_element_by_name("work").send_keys(kontakt.work)
         driver.find_element_by_name("fax").click()
         driver.find_element_by_name("fax").clear()
-        driver.find_element_by_name("fax").send_keys(fax)
+        driver.find_element_by_name("fax").send_keys(kontakt.fax)
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(email)
+        driver.find_element_by_name("email").send_keys(kontakt.email)
         driver.find_element_by_name("email2").click()
         driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys(email2)
+        driver.find_element_by_name("email2").send_keys(kontakt.email2)
         driver.find_element_by_name("email3").click()
         driver.find_element_by_name("email3").clear()
-        driver.find_element_by_name("email3").send_keys(email3)
+        driver.find_element_by_name("email3").send_keys(kontakt.email3)
         driver.find_element_by_name("homepage").click()
         driver.find_element_by_name("homepage").clear()
-        driver.find_element_by_name("homepage").send_keys(homepage)
+        driver.find_element_by_name("homepage").send_keys(kontakt.homepage)
         # choose bday
         driver.find_element_by_name("bday").click()
         Select(driver.find_element_by_name("bday")).select_by_visible_text(bday)
