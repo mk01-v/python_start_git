4c74091cf91a4a4135b2a25e2b860543a4f71237
mk01-v <prizma5150@gmail.com>
2019-11-12 17:33:06 +0700

    Перенесен обратно тест

--- test/test_AddGroupFirefox.py
+++ test/test_AddGroupFirefox.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
 def test_add_group(app):
     app.session.login(username="admin", password ="secret")
     app.group.create(Group(name="group_name_cool", header="group_header_cool_logo"))
@@ -12,7 +11,3 @@ def test_add_empty_group(app):
     app.group.create(Group(name="", header=""))
     app.session.logout()
 
-def test_delete_first_group(app):
-    app.session.login(username ="admin", password ="secret")
-    app.group.delete_first_group()
-    app.session.logout()
