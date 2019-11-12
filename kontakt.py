46f8cb5f015a9bb8a09eefb5b65232f65c5d6ced
mk01-v <prizma5150@gmail.com>
2019-11-12 10:39:35 +0700

    Добавлено: класс kontakt

---
+++ kontakt.py
@@ -0,0 +1,35 @@
+
+
+class kontakt:
+    def __init__(self, username, middle_name, last_name, nickname, title, company, address, home, mobile,
+                       work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
+                       secondary_address2, secondary_home2, secondary_notes):
+        self.username = username
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.secondary_address2 = secondary_address2
+        self.secondary_home2 = secondary_home2
+        self.secondary_notese = secondary_notes
+
+
+
+
+
