b9d9743b774e25929e32f73fad37e13e5a8b9727
mk01-v <prizma5150@gmail.com>
2019-11-12 18:04:25 +0700

    изменены кавычки

--- conftest.py
+++ conftest.py
@@ -1,9 +1,8 @@
 
 from fixture.application import application
 import pytest
-
-#@pytest.fixture(scope = "session")
-@pytest.fixture()
+@pytest.fixture(scope='session')
+#@pytest.fixture()
 def app(request):
     fixture = application()
     request.addfinalizer(fixture.destroy)
