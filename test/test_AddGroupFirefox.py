683be8d4929fb44819f8f59208be7ba34158c201
mk01-v <prizma5150@gmail.com>
2019-11-12 18:05:14 +0700

    Статус: работает. Удалены лишние пробелы.

--- test/test_AddGroupFirefox.py
+++ test/test_AddGroupFirefox.py
@@ -2,12 +2,12 @@
 from model.group import Group
 
 def test_add_group(app):
-    app.session.login(username="admin", password ="secret")
+    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="group_name_cool", header="group_header_cool_logo"))
     app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password ="secret")
+    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header=""))
     app.session.logout()
 
