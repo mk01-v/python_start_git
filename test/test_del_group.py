083dcaa848fcdfd6a693b8ed17c7633c79df6045
mk01-v <prizma5150@gmail.com>
2019-11-12 17:33:19 +0700

    Удаление пробелов.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,5 +1,5 @@
 #удаление группы.
 def test_delete_first_group(app):
-    app.session.login(username ="admin", password ="secret")
+    app.session.login(username="admin", password="secret")
     app.group.delete_first_group()
     app.session.logout()
